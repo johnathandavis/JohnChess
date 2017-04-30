b451d1dc466b9e2164805362f0925322b73bc341
Johnathan Davis <john@johnathandavis.com>
2017-04-30 15:04:06 -0700

    Added queen move logic.

--- JohnChess/Pieces/Queen.cs
+++ JohnChess/Pieces/Queen.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 using JohnChess.Moves;
 
@@ -13,7 +13,50 @@ namespace JohnChess.Pieces
 
         public override List<Move> FindMoves(Board board)
         {
-            return new List<Move>();
+            IEnumerable<Move> moves = new List<Move>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    // Don't consider the zero-magnitude, this would cause
+                    // an infinite loop!
+                    if (x == y && x == 0) continue;
+
+                    moves = moves.Concat(GetMovesInDirection(board, x, y));
+                }
+            }
+            return moves.ToList();
+        }
+
+        private List<Move> GetMovesInDirection(Board board, int vert, int horiz)
+        {
+            var moves = new List<Move>();
+            int currentRank = (int)Position.Rank;
+            int currentFile = (int)Position.File;
+            int magnitude = 1;
+
+            while (true)
+            {
+                int newRank = currentRank + magnitude * vert;
+                int newFile = currentFile + magnitude * horiz;
+                var newPos = new Position((File)newFile, (Rank)newRank);
+
+                if (!Position.IsOnBoard(newPos)) break;
+                var currentPiece = board[newPos];
+                if (currentPiece != null)
+                {
+                    if (currentPiece.Color != this.Color)
+                    {
+                        // We can take!
+                        moves.Add(MoveBuilder.CreateNormalMove(this, newPos, true));
+                    }
+                    break;
+                }
+                moves.Add(MoveBuilder.CreateNormalMove(this, newPos, false));
+                magnitude++;
+            }
+
+            return moves;
         }
     }
 }
